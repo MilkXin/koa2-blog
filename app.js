26f6a99d162511e9713cc4057b097840ce40be09
chen xin <729609320@.com>
2019-12-14 23:43:05 +0800

    日志记录

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-generic-session');
 const redisStore = require('koa-redis');
+const path = require('path')
+const fs = require('fs');
+const morgan = require('koa-morgan');
+
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -38,6 +42,22 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+const ENV = process.env.NODE_ENV
+if (ENV !== 'production') {
+  //开发或测试环境
+  app.use(morgan('dev'));
+} else {
+  //生产环境
+
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  })
+  app.use(morgan('combined', {
+    stream: writeStream
+  }));
+}
+
 //session 配置
 app.keys = ['chen#2378']
 app.use(session({
@@ -48,10 +68,10 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000
   },
   //配置redis
-  store: {
+  store: redisStore({
     // all: '127.0.0.1:6379'
     all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
-  }
+  })
 }))
 
 // routes
